3fb1af6e329a5055d1a12c6d9122980606dffcc6
evanevanich <evanevanich@hotmail.co.uk>
2024-08-23 00:33:52 +0100

    Added Ore/Metal Blocks

---
+++ src/main/java/net/evoon/LoE/item/ModCreativeModTabs.java
@@ -0,0 +1,107 @@
+package net.evoon.LoE.item;
+
+import net.evoon.LoE.LoE;
+import net.evoon.LoE.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LoE.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> LOE_TAB = CREATIVE_MODE_TABS.register("legends_of_eldoria",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ITHILANIUM.get()))
+                    .title(Component.translatable("creativetab.legends_of_eldoria"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.DWARVEN_STEEL.get());
+                        output.accept(ModItems.ITHILANIUM.get());
+                        output.accept(ModItems.ITHILANIUM_INGOT.get());
+                        output.accept(ModItems.UNCUT_QUARTZ.get());
+                        output.accept(ModItems.QUARTZ_GEM.get());
+                        output.accept(ModItems.ADAMANTITE.get());
+                        output.accept(ModItems.ADAMANTITE_INGOT.get());
+                        output.accept(ModItems.RAW_TIN.get());
+                        output.accept(ModItems.TIN_INGOT.get());
+                        output.accept(ModItems.RAW_SILVER.get());
+                        output.accept(ModItems.SILVER_INGOT.get());
+                        output.accept(ModItems.RAW_PLATINUM.get());
+                        output.accept(ModItems.PLATINUM_INGOT.get());
+                        output.accept(ModItems.UNCUT_OPAL.get());
+                        output.accept(ModItems.OPAL_GEM.get());
+                        output.accept(ModItems.UNCUT_SAPPHIRE.get());
+                        output.accept(ModItems.SAPPHIRE_GEM.get());
+                        output.accept(ModItems.UNCUT_CITRINE.get());
+                        output.accept(ModItems.CITRINE_GEM.get());
+                        output.accept(ModItems.UNCUT_CORUNDUM.get());
+                        output.accept(ModItems.CORUNDUM_GEM.get());
+                        output.accept(ModItems.UNCUT_RUBY.get());
+                        output.accept(ModItems.RUBY_GEM.get());
+                        output.accept(ModItems.UNCUT_PERIDOT.get());
+                        output.accept(ModItems.PERIDOT_GEM.get());
+                        output.accept(ModItems.BRONZE_INGOT.get());
+
+                        output.accept(ModBlocks.DWARVEN_STEEL_BLOCK.get());
+                        output.accept(ModBlocks.ITHILANIUM_BLOCK.get());
+                        output.accept(ModBlocks.RAW_ITHILANIUM_BLOCK.get());
+                        output.accept(ModBlocks.ITHILANIUM_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_ITHILANIUM_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_GLOWSTONE_ORE.get());
+                        output.accept(ModBlocks.GLOWSTONE_ORE.get());
+                        output.accept(ModBlocks.QUARTZ_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_QUARTZ_ORE.get());
+                        output.accept(ModBlocks.UNCUT_QUARTZ_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_QUARTZ_BLOCK.get());
+                        output.accept(ModBlocks.ADAMANTITE_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_ADAMANTITE_ORE.get());
+                        output.accept(ModBlocks.RAW_ADAMANTITE_BLOCK.get());
+                        output.accept(ModBlocks.ADAMANTITE_BLOCK.get());
+                        output.accept(ModBlocks.TIN_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_TIN_ORE.get());
+                        output.accept(ModBlocks.RAW_TIN_BLOCK.get());
+                        output.accept(ModBlocks.TIN_BLOCK.get());
+                        output.accept(ModBlocks.SILVER_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_SILVER_ORE.get());
+                        output.accept(ModBlocks.RAW_SILVER_BLOCK.get());
+                        output.accept(ModBlocks.SILVER_BLOCK.get());
+                        output.accept(ModBlocks.PLATINUM_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_PLATINUM_ORE.get());
+                        output.accept(ModBlocks.RAW_PLATINUM_BLOCK.get());
+                        output.accept(ModBlocks.PLATINUM_BLOCK.get());
+                        output.accept(ModBlocks.OPAL_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_OPAL_ORE.get());
+                        output.accept(ModBlocks.UNCUT_OPAL_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_OPAL_BLOCK.get());
+                        output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        output.accept(ModBlocks.UNCUT_SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.CITRINE_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_CITRINE_ORE.get());
+                        output.accept(ModBlocks.UNCUT_CITRINE_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_CITRINE_BLOCK.get());
+                        output.accept(ModBlocks.CORUNDUM_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_CORUNDUM_ORE.get());
+                        output.accept(ModBlocks.UNCUT_CORUNDUM_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_CORUNDUM_BLOCK.get());
+                        output.accept(ModBlocks.RUBY_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_RUBY_ORE.get());
+                        output.accept(ModBlocks.UNCUT_RUBY_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_RUBY_BLOCK.get());
+                        output.accept(ModBlocks.PERIDOT_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_PERIDOT_ORE.get());
+                        output.accept(ModBlocks.UNCUT_PERIDOT_BLOCK.get());
+                        output.accept(ModBlocks.REFINED_PERIDOT_BLOCK.get());
+                        output.accept(ModBlocks.BRONZE_BLOCK.get());
+
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/evoon/LoE/item/ModItems.java
@@ -0,0 +1,95 @@
+package net.evoon.LoE.item;
+
+import net.evoon.LoE.LoE;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, LoE.MOD_ID);
+
+    public static final RegistryObject<Item> ITHILANIUM = ITEMS.register("ithilanium",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ITHILANIUM_INGOT = ITEMS.register("ithilanium_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_QUARTZ = ITEMS.register("uncut_quartz",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> QUARTZ_GEM = ITEMS.register("quartz_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DWARVEN_STEEL = ITEMS.register("dwarven_steel",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONZE_INGOT = ITEMS.register("bronze_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ADAMANTITE = ITEMS.register("adamantite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ADAMANTITE_INGOT = ITEMS.register("adamantite_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_TIN = ITEMS.register("raw_tin",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_PLATINUM = ITEMS.register("raw_platinum",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PLATINUM_INGOT = ITEMS.register("platinum_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_SILVER = ITEMS.register("raw_silver",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_OPAL = ITEMS.register("uncut_opal",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> OPAL_GEM = ITEMS.register("opal_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_SAPPHIRE = ITEMS.register("uncut_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_GEM = ITEMS.register("sapphire_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_CITRINE = ITEMS.register("uncut_citrine",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CITRINE_GEM = ITEMS.register("citrine_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_CORUNDUM = ITEMS.register("uncut_corundum",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CORUNDUM_GEM = ITEMS.register("corundum_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_RUBY = ITEMS.register("uncut_ruby",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RUBY_GEM = ITEMS.register("ruby_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> UNCUT_PERIDOT = ITEMS.register("uncut_peridot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PERIDOT_GEM = ITEMS.register("peridot_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
